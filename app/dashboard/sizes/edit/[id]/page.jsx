0167f9938275966afa7205a500a502b1cb246bcf
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-02-22 23:41:57 +0600

    finish size crud

--- app/dashboard/sizes/edit/[id]/page.jsx
+++ app/dashboard/sizes/edit/[id]/page.jsx
@@ -7,6 +7,7 @@ import { toast } from 'react-toastify';
 
 export default  function page() {
     const [data, setData] = useState({size:""})
+    const [isLoading, setIsLoading] = useState(false)
     const route = useRouter();
     const params = useParams();
     const {id} = params;
@@ -24,18 +25,35 @@ export default  function page() {
       if (id) fetchSize();
    },[id])
 
+   function handleChange (e){
+      setData({...data,size: e.target.value})
+   }
+
+   async function editSize (e){
+     e.preventDefault();
+     setIsLoading(true)
+     try{
+        await axios.put(`http://127.0.0.1:8000/api/sizes/${id}`,{size:data.size})
+        toast.success("Updated Successfully")
+        window.location.href = "/dashboard/sizes"
+     }catch(err){
+       toast.error(err.response?.data?.message || "An Error Occurred")
+     }finally{
+       setIsLoading(false)
+     }
+   }
     
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
     <div className="card shadow-lg p-4" style={{ width: "400px" }}>
       <h4 className="text-center mb-4">Update Size ({id})</h4>
       
-      <form >
+      <form onSubmit={editSize}>
         <div className="form-group">
           <label htmlFor="size" className="fw-bold">Size:</label>
-          <input type="text" className="form-control" id="size" name="size" placeholder="Enter size" value={data?.size} />
+          <input type="text" className="form-control" id="size" name="size" placeholder="Enter size" value={data?.size} onChange={handleChange}/>
         </div>
-        <button type="submit" className="btn btn-primary w-100 mt-3">Update</button>
+        <button type="submit" className="btn btn-primary w-100 mt-3"> {isLoading?"...Updating":"Update"} </button>
       </form>
     </div>
   </div>
