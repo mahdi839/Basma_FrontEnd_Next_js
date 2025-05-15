cd97c47137934b393b28602b3ad0cbf707b2b816
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-05-15 13:03:19 +0600

    added protected route on dashboard

--- app/components/layouts/BackEndLayout.jsx
+++ app/components/layouts/BackEndLayout.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import SideBar from '../dashboard/sideBar/SideBar'
 import './dashboard.css'
+import { useRouter } from 'next/navigation';
 export default function BackEndLayout({children}) {
+    let router = useRouter()
+    useEffect(()=>{
+      let token = localStorage.getItem('token')
+      if(!token){
+        router.replace('/frontEnd/log_in')
+      }
+    },[router])
   return (
     <>
        <div className='Dashboard_layout'>
