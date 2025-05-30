92576f4a3346c897922ba46852c07edc0f68a682
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-05-31 01:43:43 +0600

    completed fonfigure redux toolkit

---
+++ app/redux/slices/CartSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const loadCartFromStorage = ()=>{
+    if(typeof window !== 'undefined'){
+       let storedCart = localStorage.getItem("cart");
+        return storedCart?JSON.parse(storedCart) : []
+    }
+    return [];
+}
+const initialItems = loadCartFromStorage();
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState:{
+        items: initialItems,
+        count: initialItems.length || 0
+    },
+    reducers:{
+        addToCart: (state,action)=>{
+            const {id,title,size,price,image} = action.payload;
+            const existingItem = state.items.find(item=> item.id === id)
+            if(existingItem){
+                existingItem.qty += 1;
+                existingItem.price = existingItem.price * existingItem.qty
+            }else{
+                state.items.push({
+                    id,
+                    title,
+                    size,
+                    price,
+                    image,
+                    qty:1
+                })
+            }
+            if (typeof window !== 'undefined') {
+                localStorage.setItem('cart', JSON.stringify(state.items));
+            }
+        }
+    }
+})
+
+const {addToCart} = cartSlice.actions
+export default cartSlice.reducer;
