a793f4a5d916b2a44d0eee8a400efd7cd423ccac
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-07-25 03:34:50 +0600

    show banners details

---
+++ app/dashboard/banners/add/page.jsx
@@ -0,0 +1,217 @@
+"use client";
+import useIndexData from "@/app/hooks/useIndexData";
+import useStoreData from "@/app/hooks/useStoreData";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+
+export default function Page() {
+  const [formData, setFormData] = useState({
+    link: "",
+    type: "",
+    products_slots_id: "",
+    category_id: "",
+    images: [],
+  });
+  const [slot, setSlot] = useState(null);
+  const { indexData, loading, data, setData } = useIndexData();
+  const categoryIndeUrl = process.env.BACKEND_URL + `api/categories`;
+  const slotUrl = process.env.BACKEND_URL + `api/product-slots`;
+
+  async function getSlotData() {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.get(slotUrl, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setSlot(response.data.data);
+    } catch (err) {
+      Swal.fire({
+        title: "Oops! Please Check",
+        text: err.response?.data?.message || err.message,
+        icon: "error",
+      });
+    }
+  }
+
+  useEffect(() => {
+    indexData(categoryIndeUrl);
+    getSlotData();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleChangeFileChange = (e) => {
+    setFormData({ ...formData, images: e.target.files });
+  };
+
+  const { storeData, loading: storeLoading } = useStoreData();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const payload = new FormData();
+    payload.append("link", formData.link);
+    payload.append("type", formData.type);
+    if (formData.type === "slot") {
+      payload.append("products_slots_id", formData.products_slots_id);
+    }
+
+    if (formData.type === "category") {
+      payload.append("category_id", formData.category_id);
+    }
+
+    for (let i = 0; i < formData.images.length; i++) {
+      payload.append(`images[${i}]`, formData.images[i]);
+    }
+    await storeData(
+      process.env.BACKEND_URL + "api/banners",
+      payload,
+      "Banner created successfully"
+    );
+
+    setFormData({
+      link: "",
+      type: "",
+      category_id: "",
+      images: [],
+    });
+
+    document.getElementById("images").value = null;
+  };
+
+  return (
+    <div className="d-flex align-items-center justify-content-center min-vh-100 bg-light p-3">
+      <div
+        className="card shadow-lg rounded-3 border-0 w-100"
+        style={{ maxWidth: "800px" }}
+      >
+        <div
+          className="card-header  text-white py-3 rounded-top-3"
+          style={{ background: "#7d59bf" }}
+        >
+          <h5 className="mb-0 text-center">Add Banner Images</h5>
+        </div>
+
+        <div className="card-body p-4">
+          <form onSubmit={handleSubmit}>
+            <div className="row g-4">
+              {/* Slot Name */}
+              <div
+                className={`${
+                  formData.type === "category" || "slot"
+                    ? "col-md-6"
+                    : "col-md-12"
+                }`}
+              >
+                <div className="">
+                  <select
+                    name="type"
+                    className="form-select border-secondary"
+                    value={formData.type}
+                    onChange={handleChange}
+                  >
+                    <option value="" disabled>
+                      Select Type
+                    </option>
+                    <option value="hero">Hero Section Slider</option>
+                    <option value="slot">Slot</option>
+                    <option value="category">Category</option>
+                  </select>
+                </div>
+              </div>
+
+              {formData.type === "category" && (
+                <div className="col-md-6">
+                  <div className="">
+                    <select
+                      className="form-select border-secondary"
+                      name="category_id"
+                      value={formData.category_id}
+                      onChange={handleChange}
+                    >
+                      <option value="" disabled>
+                        Select category
+                      </option>
+                      {data?.data?.map((category) => (
+                        <option key={category.id} value={category.id}>
+                          {category.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+              )}
+
+              {formData.type === "slot" && (
+                <div className="col-md-6">
+                  <div className="">
+                    <select
+                      className="form-select border-secondary"
+                      name="products_slots_id"
+                      value={formData.products_slots_id}
+                      onChange={handleChange}
+                    >
+                      <option value="" disabled>
+                        Select Slot
+                      </option>
+                      {slot?.map((singleSlot) => (
+                        <option key={singleSlot.id} value={singleSlot.id}>
+                          {singleSlot.slot_name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+              )}
+
+              {/* images */}
+              <div className="col-md-6">
+                <div className="form">
+                  <input
+                    type="file"
+                    className="form-control border-secondary"
+                    id="images"
+                    placeholder="Select Image"
+                    name="images"
+                    multiple={formData.type == "hero"}
+                    required
+                    onChange={handleChangeFileChange}
+                  />
+                </div>
+              </div>
+
+              {/* links */}
+              <div className="col-md-6">
+                <div className="form">
+                  <input
+                    type="text"
+                    className="form-control border-secondary"
+                    id="link"
+                    placeholder="Give link"
+                    name="link"
+                    value={formData.link}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* Submit Button */}
+            <div className="mt-5 pt-3 border-top col-12">
+              <div className="d-flex gap-2 justify-content-center">
+                <button type="submit" className="dashboard-btn">
+                  Save
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/dashboard/banners/page.jsx
+++ app/dashboard/banners/page.jsx
@@ -1,217 +1,97 @@
-"use client";
-import useIndexData from "@/app/hooks/useIndexData";
-import useStoreData from "@/app/hooks/useStoreData";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import Swal from "sweetalert2";
+'use client'
+import PageLoader from '@/app/components/loader/pageLoader';
+import useDeleteItem from '@/app/hooks/useDeleteItem';
+import useIndexData from '@/app/hooks/useIndexData'
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react'
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import Pagination from '../orders/components/Pagination';
+import Image from 'next/image';
 
-export default function Page() {
-  const [formData, setFormData] = useState({
-    link: "",
-    type: "",
-    products_slots_id: "",
-    category_id: "",
-    images: [],
-  });
-  const [slot, setSlot] = useState(null);
-  const { indexData, loading, data, setData } = useIndexData();
-  const categoryIndeUrl = process.env.BACKEND_URL + `api/categories`;
-  const slotUrl = process.env.BACKEND_URL + `api/product-slots`;
+export default function page() {
+  const [page, setPage] = useState(1);
 
-  async function getSlotData() {
-    const token = localStorage.getItem("token");
-    try {
-      const response = await axios.get(slotUrl, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setSlot(response.data.data);
-    } catch (err) {
-      Swal.fire({
-        title: "Oops! Please Check",
-        text: err.response?.data?.message || err.message,
-        icon: "error",
-      });
-    }
+  const {indexData,loading,data,setData,pagination,setPagination} = useIndexData();
+  const bannerUrl = process.env.BACKEND_URL + `api/banners?page=${page}`;
+  useEffect(()=>{
+     indexData(bannerUrl)
+  },[page])
+  
+  const {handleDeleteData,deleteLoading} = useDeleteItem();
+   function handleDelete (id){
+      const url = process.env.BACKEND_URL + `api/banners/${id}`
+      
+      handleDeleteData(url)
+      setData(
+        {
+          ...data,
+          data:data.data.filter((slot)=> slot.id !=id)
+        }
+        )
+      
+   }
+
+  if(loading){
+    return <PageLoader />
   }
-
-  useEffect(() => {
-    indexData(categoryIndeUrl);
-    getSlotData();
-  }, []);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleChangeFileChange = (e) => {
-    setFormData({ ...formData, images: e.target.files });
-  };
-
-  const { storeData, loading: storeLoading } = useStoreData();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const payload = new FormData();
-    payload.append("link", formData.link);
-    payload.append("type", formData.type);
-    if (formData.type === "slot") {
-      payload.append("products_slots_id", formData.products_slots_id);
-    }
-
-    if (formData.type === "category") {
-      payload.append("category_id", formData.category_id);
-    }
-
-    for (let i = 0; i < formData.images.length; i++) {
-      payload.append(`images[${i}]`, formData.images[i]);
-    }
-    await storeData(
-      process.env.BACKEND_URL + "api/banners",
-      payload,
-      "Banner created successfully"
-    );
-
-    setFormData({
-      link: "",
-      type: "",
-      category_id: "",
-      images: [],
-    });
-
-    document.getElementById("images").value = null;
-  };
-
   return (
-    <div className="d-flex align-items-center justify-content-center min-vh-100 bg-light p-3">
-      <div
-        className="card shadow-lg rounded-3 border-0 w-100"
-        style={{ maxWidth: "800px" }}
-      >
-        <div
-          className="card-header  text-white py-3 rounded-top-3"
-          style={{ background: "#7d59bf" }}
-        >
-          <h5 className="mb-0 text-center">Add Banner Images</h5>
-        </div>
+    <div className='container-fluid my-5'>
+      <Link href="/dashboard/banners/add">
+         <button className='dashboard-btn mb-3'>Create Banner</button>
+      </Link>
+         <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th className="text-center">#</th>
+          <th className="text-center">Link</th>
+          <th className="text-center">Banner Type</th>
+          <th className="text-center">Slot</th>
+          <th className="text-center">Category</th>
+          <th className="text-center">Image</th>
+          {/* <th className="text-center">Actions</th> */}
+        </tr>
+      </thead>
+      <tbody>
+        {data.data.length === 0 ? (
+          <tr>
+            <td colSpan="5" className="text-center text-danger">
+              No Banner Available
+            </td>
+          </tr>
+        ) : (
+          data.data.map((banner,index) => (
+            <tr key={banner.id}>
+              <td className="text-center">{index+1}</td>
+              <td className="text-center">{banner.link}</td>
+              <td className="text-center">{banner.type}</td>
+              
+              <td className="text-center">  {banner?.slot?.slot_name}</td>
+              <td className="text-center">  {banner?.category?.name?? "N/A"}</td>
+              <td className="text-center">  {banner?.banner_mages?.map((img)=>(
+                <Image className='ml-2 rounded' src={`${process.env.BACKEND_URL}storage/${img.path}`} width={50} height={50} />
+              ))}</td>
+              {/* <td className="text-center">
+                <span className="d-flex gap-3 justify-content-center ">
+                  <Link href={`/dashboard/slots/edit/${slot.id}`}>
+                    <FaEdit className="text-dark"/>
+                  </Link>
+                  
+                  <FaTrash className="text-danger mt-2" onClick={() => handleDelete(slot.id)} style={{cursor:'pointer'}} />
+                </span>
+              </td> */}
+            </tr>
+          ))
+        )}
+      </tbody>
+    </table>
 
-        <div className="card-body p-4">
-          <form onSubmit={handleSubmit}>
-            <div className="row g-4">
-              {/* Slot Name */}
-              <div
-                className={`${
-                  formData.type === "category" || "slot"
-                    ? "col-md-6"
-                    : "col-md-12"
-                }`}
-              >
-                <div className="">
-                  <select
-                    name="type"
-                    className="form-select border-secondary"
-                    value={formData.type}
-                    onChange={handleChange}
-                  >
-                    <option value="" disabled>
-                      Select Type
-                    </option>
-                    <option value="hero">Hero Section Slider</option>
-                    <option value="slot">Slot</option>
-                    <option value="category">Category</option>
-                  </select>
-                </div>
-              </div>
-
-              {formData.type === "category" && (
-                <div className="col-md-6">
-                  <div className="">
-                    <select
-                      className="form-select border-secondary"
-                      name="category_id"
-                      value={formData.category_id}
-                      onChange={handleChange}
-                    >
-                      <option value="" disabled>
-                        Select category
-                      </option>
-                      {data?.data?.map((category) => (
-                        <option key={category.id} value={category.id}>
-                          {category.name}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                </div>
-              )}
-
-              {formData.type === "slot" && (
-                <div className="col-md-6">
-                  <div className="">
-                    <select
-                      className="form-select border-secondary"
-                      name="products_slots_id"
-                      value={formData.products_slots_id}
-                      onChange={handleChange}
-                    >
-                      <option value="" disabled>
-                        Select Slot
-                      </option>
-                      {slot?.map((singleSlot) => (
-                        <option key={singleSlot.id} value={singleSlot.id}>
-                          {singleSlot.slot_name}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                </div>
-              )}
-
-              {/* images */}
-              <div className="col-md-6">
-                <div className="form">
-                  <input
-                    type="file"
-                    className="form-control border-secondary"
-                    id="images"
-                    placeholder="Select Image"
-                    name="images"
-                    multiple={formData.type == "hero"}
-                    required
-                    onChange={handleChangeFileChange}
-                  />
-                </div>
-              </div>
-
-              {/* links */}
-              <div className="col-md-6">
-                <div className="form">
-                  <input
-                    type="text"
-                    className="form-control border-secondary"
-                    id="link"
-                    placeholder="Give link"
-                    name="link"
-                    value={formData.link}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
-            </div>
-
-            {/* Submit Button */}
-            <div className="mt-5 pt-3 border-top col-12">
-              <div className="d-flex gap-2 justify-content-center">
-                <button type="submit" className="dashboard-btn">
-                  Save
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+ {pagination.last_page > 1 && (
+        <Pagination 
+          page={page}
+          setPage={setPage}
+          pagination={pagination}
+        />
+      )}
     </div>
-  );
+  )
 }
