4e07de58a51cb7742898642f35c5cc7c85bcba1b
mahdi839 <77610054+mahdi839@users.noreply.github.com>
2025-08-01 22:01:25 +0600

    footer design completed

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -6,168 +6,132 @@ import {
   FaInstagram,
   FaYoutube,
   FaWhatsappSquare,
+  FaPhone,
 } from "react-icons/fa";
+import { CiLocationOn, CiMail } from "react-icons/ci";
+
 export default function Footer() {
   return (
-    <section className="bg-warning-subtle py-5 position-relative overflow-hidden">
-      <img
-        className="position-absolute bottom-0 start-0"
-        src="/img/footer/footer_wave_img2.png"
-        alt=""
-      />
-      
+    <footer className="bg-warning-subtle position-relative overflow-hidden pt-5">
+      {/* Top curve */}
+     
 
-      <div className="container position-relative">
-        <div className="row mb-5">
-          <div className="col-lg-2 mb-4 mb-lg-0">
-            <Link className="d-inline-block mb-3" href="#">
-              <img src="/images/logo.svg" alt="" />
-            </Link>
-          </div>
-
-          <div className="col-md-7 col-lg-6 mb-4 mb-lg-0">
-            <div className="row">
-              <div className="col-6 col-sm-4 mb-3">
-                <h5 className="fw-bold mb-3">Platform</h5>
-                <ul className="list-unstyled">
-                  <li>
-                    <Link
-                      className="text-secondary text-decoration-none d-block mb-2"
-                      href="#"
-                    >
-                      Solutions
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="text-secondary text-decoration-none d-block mb-2"
-                      href="#"
-                    >
-                      How it works
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="text-secondary text-decoration-none d-block"
-                      href="#"
-                    >
-                      Pricing
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-6 col-sm-4 mb-3">
-                <h5 className="fw-bold mb-3">Resources</h5>
-                <ul className="list-unstyled">
-                  <li>
-                    <Link
-                      className="text-secondary text-decoration-none d-block mb-2"
-                      href="#"
-                    >
-                      Blog
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="text-secondary text-decoration-none d-block mb-2"
-                      href="#"
-                    >
-                      Help Center
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="text-secondary text-decoration-none d-block"
-                      href="#"
-                    >
-                      Support
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-12 col-sm-4">
-                <h5 className="fw-bold mb-3">Company</h5>
-                <ul className="list-unstyled">
-                  <li>
-                    <Link
-                      className="text-secondary text-decoration-none d-block mb-2"
-                      href="#"
-                    >
-                      About
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="text-secondary text-decoration-none d-block mb-2"
-                      href="#"
-                    >
-                      Our Mission
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="text-secondary text-decoration-none d-block mb-2"
-                      href="#"
-                    >
-                      Careers
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="text-secondary text-decoration-none d-block"
-                      href="#"
-                    >
-                      Contact
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-md-5 col-lg-4">
-            <div className="bg-dark text-white rounded-3 p-4">
-              <h5 className="mb-3">Your Source for Green Energy Updates</h5>
-              <p className="small opacity-75 mb-4">
-                Stay in the loop with our Green Horizon newsletter, where we
-                deliver bite-sized insights into the latest green energy
-                solutions.
-              </p>
-              <div className="d-flex flex-column">
-                <input
-                  type="email"
-                  className="form-control mb-2 rounded-pill"
-                  placeholder="Your e-mail..."
+      <div className="container position-relative py-4">
+        <div className="row g-4">
+          {/* Logo Section */}
+          <div className="col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <Link href="#" className="d-inline-block mb-4">
+              <div className="bg-white p-2 rounded shadow-sm d-inline-block">
+                <img 
+                  src="/images/logo.svg" 
+                  alt="Company Logo" 
+                  className="img-fluid"
+                  style={{ height: "40px" }}
                 />
-                <Link
-                  href="#"
-                  className="btn btn-warning text-success fw-semibold rounded-pill"
-                >
-                  Get in touch
-                </Link>
               </div>
+            </Link>
+            <p className="text-muted mb-4">
+              Providing premium products with exceptional customer service since 2010. We're committed to quality and customer satisfaction.
+            </p>
+            <div className="d-flex gap-3">
+              <Link href="#" className="text-dark fs-5">
+                <FaFacebook />
+              </Link>
+              <Link href="#" className="text-dark fs-5">
+                <FaInstagram />
+              </Link>
+              <Link href="#" className="text-dark fs-5">
+                <FaWhatsappSquare />
+              </Link>
+              <Link href="#" className="text-dark fs-5">
+                <FaYoutube />
+              </Link>
             </div>
           </div>
+
+          {/* Store Information */}
+          <div className="col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <h5 className="fw-bold mb-4  pb-2">Store Information</h5>
+            <ul className="list-unstyled text-muted">
+              <li className="mb-3 d-flex">
+                <CiLocationOn className="text-dark mt-1 me-3 fs-5" />
+                <span>
+                  My Company, 42 Puffin street<br />12345 Puffinville France
+                </span>
+              </li>
+              <li className="mb-3 d-flex">
+                <FaPhone className="text-dark mt-1 me-3 fs-5" />
+                <span>01795802507</span>
+              </li>
+              <li className="d-flex">
+                <CiMail className="text-dark mt-1 me-3 fs-5" />
+                <span>hasanarefi56574@gmail.com</span>
+              </li>
+            </ul>
+          </div>
+
+          {/* Account Links */}
+          <div className="col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <h5 className="fw-bold mb-4  pb-2">My Account</h5>
+            <ul className="list-unstyled text-muted">
+              <li className="mb-2">
+                <Link href="#" className="text-muted text-decoration-none d-block mb-3">
+                  My Profile
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link href="#" className="text-muted text-decoration-none d-block mb-3">
+                  Login/Register
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="text-muted text-decoration-none d-block">
+                  My Orders
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* Useful Links */}
+          <div className="col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <h5 className="fw-bold mb-4  pb-2">Useful Links</h5>
+            <ul className="list-unstyled text-muted">
+              <li className="mb-2">
+                <Link href="#" className="text-muted text-decoration-none d-block mb-3">
+                  About
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link href="#" className="text-muted text-decoration-none d-block mb-3">
+                  Privacy Policy
+                </Link>
+              </li>
+              <li className="mb-2">
+                <Link href="#" className="text-muted text-decoration-none d-block mb-3">
+                  Terms & Condition
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="text-muted text-decoration-none d-block">
+                  FAQ
+                </Link>
+              </li>
+            </ul>
+          </div>
+
         </div>
 
-        <div className="d-flex flex-wrap justify-content-between align-items-center border-top pt-3">
-          <div className="mb-3 d-flex">
-            <Link href="#" className="text-dark me-3">
-              <FaFacebook size={25} />
-            </Link>
-            <Link href="#" className="text-dark me-3">
-              <FaInstagram size={25} />
-            </Link>
-            <Link href="#" className="text-dark me-3">
-              <FaWhatsappSquare size={25} />
-            </Link>
-             <Link href="#" className="text-dark">
-              <FaYoutube size={25} />
-            </Link>
-          </div>
-       
+        {/* Divider */}
+        <hr className="my-5 opacity-25" />
+
+        {/* Copyright */}
+        <div className="d-flex flex-wrap align-items-center justify-content-center pb-3">
+          <p className="text-muted mb-0 me-3">
+            &copy; {new Date().getFullYear()} My Company. All rights reserved.
+          </p>
+         
         </div>
       </div>
-    </section>
+    </footer>
   );
 }
