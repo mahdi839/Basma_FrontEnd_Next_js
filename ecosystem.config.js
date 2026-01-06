1503590d840e244e290a7db9ff03020fa04212c1
mahdi839 <hasanarefi56574@gmail.com>
2026-01-07 02:07:33 +0600

    unwanted things gillam

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,16 +7,7 @@ module.exports = {
     exec_mode: 'cluster',
     env: {
       NODE_ENV: 'production',
-      PORT: 3000,
-      HOSTNAME: '0.0.0.0'
-    },
-    error_file: '/home/eyarafashion/logs/pm2-error.log',
-    out_file: '/home/eyarafashion/logs/pm2-out.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-    merge_logs: true,
-    autorestart: true,
-    max_restarts: 10,
-    min_uptime: '5s',
-    watch: false
+      PORT: 3000
+    }
   }]
 }
