a1221aa4a485ec4998533a10e06483726f6a18fd
mahdi839 <hasanarefi56574@gmail.com>
2026-03-28 00:56:16 +0600

    added active menu border

--- app/components/frontEnd/nabvar/components/DesktopNav.jsx
+++ app/components/frontEnd/nabvar/components/DesktopNav.jsx
@@ -1,15 +1,40 @@
-// app/components/navbar/components/DesktopNav.jsx
-// ✅ SERVER COMPONENT — static links only
+"use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function DesktopNav() {
+  const pathname = usePathname();
+
   return (
     <nav className="header__menu">
-      <ul className="d-flex justify-content-center align-items-center mb-0">
-        <li className="mx-3"><Link href="/">Home</Link></li>
-        <li className="mx-3"><Link href="/frontEnd/shop">Shop</Link></li>
-        <li className="mx-3"><Link href="/frontEnd/about_us">About Us</Link></li>
+      <ul className="d-flex justify-content-center align-items-center mb-0 nav-list">
+        <li className="mx-3">
+          <Link
+            href="/"
+            className={`desktop-nav-link ${pathname == "/" ? "activated-nav" : ""}`}
+          >
+            Home
+          </Link>
+        </li>
+
+        <li className="mx-3">
+          <Link
+            href="/frontEnd/shop"
+            className={`desktop-nav-link ${pathname == "/frontEnd/shop" ? "activated-nav" : ""}`}
+          >
+            Shop
+          </Link>
+        </li>
+
+        <li className="mx-3">
+          <Link
+            href="/frontEnd/about_us"
+            className={`desktop-nav-link ${pathname == "/frontEnd/about_us" ? "activated-nav" : ""}`}
+          >
+            About Us
+          </Link>
+        </li>
       </ul>
     </nav>
   );
