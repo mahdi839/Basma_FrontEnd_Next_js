d1d9fe04252b621aca1c1e588f428817b3b71002
Mehedi Hasan <hasanarefi56574@gmail.com>
2026-03-31 18:15:13 +0600

    modify hero banners and optimize it

--- app/components/frontEnd/home/hero/hero.js
+++ app/components/frontEnd/home/hero/hero.js
@@ -1,15 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { useMemo } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Slider from "react-slick";
 import Image from "next/image";
 import style from "../../nabvar/hero.module.css";
 import Link from "next/link";
-
+import { heroImages } from "./components/heroData";
 export default function Hero() {
 
-  const settings = {
+  const settings = useMemo(() => ({
     dots: false,
     infinite: true,
     speed: 1100,
@@ -28,35 +28,21 @@ export default function Hero() {
         <FaChevronLeft className={style.arrow_left} />
       </div>
     ),
-  };
+  }), []);
 
-  const images = [
-    {
-      id:1,
-      url:'/img/hero_banners/premium shoes-01.jpg',
-      link:'/frontEnd/women-shoes-collection',
-    },
-    {
-      id:2,
-      url:'/img/hero_banners/summer collection-01.jpg'
-    },
-    {
-      id:3,
-      url:'/img/hero_banners/winter collection-01.jpg'
-    }
-  ]
+
 
   return (
     <div className="container">
       <Slider {...settings}>
-        {images.map((img, index) => {
+        {heroImages.map((img, index) => {
           const imageEl = (
             <Image
               className="hero_slider_img"
               src={img.url}
               width={1200}
               height={400}
-              alt="Banner"
+              alt={img.alt}
               priority={index === 0}
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 1200px"
             />
