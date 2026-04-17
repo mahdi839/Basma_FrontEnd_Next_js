c4ffa6e2d252169b0996e7f9e3f5bdff9ec8093e
mahdi839 <hasanarefi56574@gmail.com>
2026-04-17 16:51:32 +0600

    modify ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,8 @@ module.exports = {
       cwd: "/home/eyarafashion/public_html",
       script: "npm",
       args: "start",
+      instances: 2,
+      exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
         PORT: 3000
